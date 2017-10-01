7144b3b92c47714ee881bdf481ec24d8aea397f5
Sye <morrowind789@gmail.com>
2017-10-01 18:43:21 -0500

    post last week demo cleanup

--- app/actions/Authentication.tsx
+++ app/actions/Authentication.tsx
@@ -7,7 +7,9 @@ import { IState } from '../reducers/Root';
 import { BackendSingleton as Backend } from '../api/backend';
 import { RootActionCreators } from './Root';
 
-// Prepare action creator
+
+
+// Prepare Action Creator
 const actionCreatorAuth = actionCreatorFactory("Authentication");
 
 // Action Payload Generic Types
@@ -32,21 +34,25 @@ const AuthenticateBasic = actionCreatorAuth.async<
 // Thunks
 const authenticateBasicWorker = bindThunkAction(AuthenticateBasic,
 	async (params, dispatch, getState, extraArg) => { // `extraArg` is always `any` (for now)
-		let result: boolean = undefined;
+		let authSuccess: boolean = undefined;
 		let session: string = undefined;
 
-		const request = Backend.api.authSimpleGet()
-		await request.done((payload) => result = payload.body.success)
-		
-		// On successfull authentication, store session and update authentication status
-		if (result) {
-			dispatch(RootActionCreators.updateAuthenticated(result))
-			//dispatch(SetSessionKey())
+		// Make request to backend
+		await Backend.api.authSimpleGet()
+			.then((payload) => authSuccess = payload.body.success)
+			.error((e) => console.error("Authentication Request Failed with: "+ e));
+
+		// Successfull Authentication
+		if (authSuccess) {
+			// Update UI authentication status
+			dispatch(RootActionCreators.updateAuthenticated(authSuccess))
+
+			// Store session key with backend
+			//dispatch(SetSessionKey(session))
 		}
 
-		return { success: result };
+		return { success: authSuccess };
 	});
-
 
 // Bundle and export action creators
 export const AuthenticationActionCreators = {
